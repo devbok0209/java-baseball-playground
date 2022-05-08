b66bedd62f0fba5910672bfa9d0e9d6ecfceb595
devbok0209 <devbok0209@bodycodi.com>
2022-05-09 02:49:25 +0900

    이 주석 제거해

--- src/main/java/baseball/run/Run.java
+++ src/main/java/baseball/run/Run.java
@@ -8,7 +8,7 @@ public class Run {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         inputView.startGame();
-        //이 주석은 나중에 제거 합니다!
+        //나나나
     }
 
 }
