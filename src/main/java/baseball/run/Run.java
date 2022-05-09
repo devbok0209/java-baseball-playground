46ebbf8bd985e4f34667d800a185e9486dd875ed
devbok0209 <devbok0209@bodycodi.com>
2022-05-09 11:24:30 +0900

    메인 커밋최종

--- src/main/java/baseball/run/Run.java
+++ src/main/java/baseball/run/Run.java
@@ -8,7 +8,7 @@ public class Run {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         inputView.startGame();
-        //나나나
+        //나나나12312312
     }
 
 }
